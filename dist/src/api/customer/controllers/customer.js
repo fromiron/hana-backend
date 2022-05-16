5a2e169a12ce55d52f0ccd820859d8018585dafe
Grayson Lee <artless1112@gmail.com>
2022-05-16 20:13:51 +0900

    Customerを削除するとデータを残し、deleted_at　flgだけ立てる ユーザーを生成するとき、性別を指定しなければ別姓(ID:3)をデフォルトにする

--- dist/src/api/customer/controllers/customer.js
+++ dist/src/api/customer/controllers/customer.js
@@ -4,4 +4,31 @@
  */
 Object.defineProperty(exports, "__esModule", { value: true });
 const strapi_1 = require("@strapi/strapi");
-exports.default = strapi_1.factories.createCoreController('api::customer.customer');
+const CUSTOMER_SEX_TYPES = {
+    male: 1,
+    female: 2,
+    other: 3
+};
+// @ts-ignore
+exports.default = strapi_1.factories.createCoreController('api::customer.customer', ({ strapi }) => ({
+    async create(ctx) {
+        const sex = ctx.request.body.data.sex;
+        strapi.log.debug(sex);
+        //性別を指定しない場合、otherに基本設定する。null undefinedにも対応
+        if (sex !== CUSTOMER_SEX_TYPES.male && sex !== CUSTOMER_SEX_TYPES.female) {
+            ctx.request.body.data.sex = CUSTOMER_SEX_TYPES.other;
+        }
+        return await super.create(ctx);
+    },
+    async delete(ctx) {
+        const { id } = ctx.params;
+        return await strapi.db.query("api::customer.customer").update({
+            where: {
+                id: id,
+            },
+            data: {
+                deleted_at: new Date()
+            },
+        });
+    },
+}));
--- src/api/customer/controllers/customer.ts
+++ src/api/customer/controllers/customer.ts
@@ -2,6 +2,36 @@
  *  customer controller
  */
 
-import { factories } from '@strapi/strapi'
+import {factories} from '@strapi/strapi'
 
-export default factories.createCoreController('api::customer.customer');
+const CUSTOMER_SEX_TYPES = {
+  male: 1,
+  female: 2,
+  other: 3
+}
+
+// @ts-ignore
+export default factories.createCoreController('api::customer.customer', ({strapi}) => ({
+
+  async create(ctx) {
+    const sex = ctx.request.body.data.sex;
+    strapi.log.debug(sex);
+    //性別を指定しない場合、otherに基本設定する。null undefinedにも対応
+    if (sex !== CUSTOMER_SEX_TYPES.male && sex !== CUSTOMER_SEX_TYPES.female) {
+      ctx.request.body.data.sex = CUSTOMER_SEX_TYPES.other
+    }
+    return await super.create(ctx);
+  },
+
+  async delete(ctx) {
+    const {id} = ctx.params;
+    return await strapi.db.query("api::customer.customer").update({
+      where: {
+        id: id,
+      },
+      data: {
+        deleted_at: new Date()
+      },
+    });
+  },
+}))
